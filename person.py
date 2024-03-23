aeaf1a956020c4dbbc64a9eecf4343015c2c6624
kanila27 <160527341+kanila27@users.noreply.github.com>
2024-03-23 01:51:10 -0700

    Update person.py

--- person.py
+++ person.py
@@ -8,7 +8,7 @@ class Person:
         print(f"Hello, my name is {self.name}. I am {self.age} years old and I am {self.gender}.")
 
 # Creating an instance of the Person class
-person1 = Person("John", 25, "male")
+person1 = Person("Hillary", 20, "male")
 
 # Calling the introduce method to display the person's information
 person1.introduce()
